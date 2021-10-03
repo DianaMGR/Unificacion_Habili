02d51d3aef3bede9ab74253c6754cc8becb37723
Diana Gonzalez <dianagr2005@hotmail.com>
2021-10-03 16:33:34 -0500

    aregando react, agregando login

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+
+import "./styles/estilos.css";
+import correo from './media/mail.ico'
+import llave from './media/llave.ico'
+import logo from './media/M.png'
+import gmail from './media/gmail.ico'
+function App() {
+  return (
+    <div className="App">
+    <body>
+ <form className="formulario">
+    <div className='contenedor-imagen'> 
+    <img src={logo} className="logo"/>
+    
+    </div>     
+     <h1>Accede al sistema desde una cuenta</h1>
+     <div className="contenedor">
+               
+         <AccesoUsuarios nombre='Correo Electronico' imagen={correo}/> 
+         
+         <AccesoUsuarios nombre='Contraseña'imagen={llave}/> 
+         
+         
+         <span>O inicia sesion con </span>
+         <div className='contenedor-gmail'>
+          <a href="https://www.gmail.com/mail/help/intl/es/about.html?iframe " > 
+          <img src={gmail} className="correo"/>
+          </a>
+    
+        </div> 
+         <input type="submit" value="Continuar" className="button"/>
+         <p>Estas aceptando los terminos y condiciones al presionar el boton Continuar.</p>
+         <p>¿Olvidaste tu Contraseña?</p>
+         
+         <p>¿No tienes una cuenta?<a className="link" href="loginvista.html">Registrate</a></p>
+     </div>
+    </form>
+</body>
+<footer></footer>
+    </div>
+    );
+    }
+function AccesoUsuarios(props){
+
+  return (
+    <div className="input-contenedor">
+    <img src={props.imagen} className="icon"/>
+    <input type="password" placeholder={props.nombre}/>
+             
+     </div>
+  )  
+}
+export default App;
